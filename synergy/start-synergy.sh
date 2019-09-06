35277b353e3d687e9d69b73340a83fbecf7d9472
MK <manojpratnas@gmail.com>
2019-09-06 19:16:01 +0100

    synergy script
    command line script to manually start synergy server

---
+++ synergy/start-synergy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/usr/bin/synergys -f  --debug INFO --name chintu-pc-dcu --enable-drag-drop --profile-dir /var/lib/synergy --log /var/log/synergy/synergy-core.log -c /home/chintu/synergy-ua --address :24800 &
