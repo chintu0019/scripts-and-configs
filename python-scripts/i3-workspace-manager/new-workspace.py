aeecbb5cb3098f2d05720b2fda2c2bf43f381322
Manoj Kesavulu <manojpratnas@gmail.com>
2023-07-11 14:55:29 +0100

    Update new-workspace.py

--- python-scripts/i3-workspace-manager/new-workspace.py
+++ python-scripts/i3-workspace-manager/new-workspace.py
@@ -10,25 +10,25 @@ Email: manojpratnas@gmail.com
 import os
 import sys
 
-# function to get the total number of workspaces using i3-msg
-def get_total_workspaces():
-    # get the total number of workspaces
-    total_workspaces = os.popen("i3-msg -t get_workspaces | jq '. | length'").read()
+#function to get the largest number of workspace using i3-msg
+def get_largest_workspace():
+    # get the largest number of workspace
+    largest_workspace = os.popen("i3-msg -t get_workspaces | jq '.[].num' | sort -n | tail -n 1").read()
     # convert the string to integer
-    total_workspaces = int(total_workspaces)
-    return total_workspaces
+    largest_workspace = int(largest_workspace)
+    return largest_workspace
 
 # function to create a new workspace using i3-msg
 def create_new_workspace():
     # get the total number of workspaces
-    total_workspaces = get_total_workspaces()
+    total_workspaces = get_largest_workspace()
     # create a new workspace
     os.system("i3-msg workspace " + str(total_workspaces + 1))
 
 # function to move current container to the next workspace using i3-msg and focus on it
 def move_container_to_next_workspace():
     # get the total number of workspaces
-    total_workspaces = get_total_workspaces()
+    total_workspaces = get_largest_workspace()
     # move the current container to the next workspace
     os.system("i3-msg move container to workspace " + str(total_workspaces + 1))
     # focus on the next workspace
